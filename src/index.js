a180bb7b2527fafa3ce57e2c11ccfd908c1f40c4
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-02-15 15:42:49 +0200

    выполнил половину работы

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
-import './css/styles.css';
+import "./css/styles.css";
+import { fetchCountries } from "./java-script/fetchCountries";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+const debounce = require("lodash.debounce");
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.querySelector("#search-box"),
+  list: document.querySelector(".country-list"),
+  div: document.querySelector(".country-info"),
+};
+
+refs.input.addEventListener("input", debounce(handleInputCountr, DEBOUNCE_DELAY));
+
+function handleInputCountr(event) {
+  const valueInput = event.target.value.trim();
+
+  if (!valueInput) {
+    return;
+  }
+  fetchCountries(valueInput).then((response) => {
+    console.log(response);
+    if (response.length === 1) {
+      renderCountry(response);
+    }
+    if (response.length < 10) {
+      ``;
+    }
+  });
+}
+
+function renderCountry(array) {
+  const arrayCountry = array
+    .map(({ name, capital, population, flags, languages }) => {
+      return `<div>
+  <img src="${flags.svg}" alt="${name.official}" width=50px/>
+</div>
+<h2>${name.official}</h2>
+<span>Capital:${capital}</span>
+<span>Population:${population}</span>
+<span>Languages:${Object.values(languages).join(", ")}</span>`;
+    })
+    .join();
+  refs.div.insertAdjacentHTML("afterbegin", arrayCountry);
+}
---
+++ src/java-script/fetchCountries.js
@@ -0,0 +1,14 @@
+const BASE_URL = "https://restcountries.com/v3.1/name/";
+
+const countryParam = `?fields=name,capital,population,flags,languages`;
+
+const fetchCountries = (name) => {
+  return fetch(`${BASE_URL}${name}${countryParam}`).then((responce) => {
+    if (!responce.ok) {
+      throw new Error(responce.message);
+    } else {
+      return responce.json();
+    }
+  });
+};
+export { fetchCountries };
